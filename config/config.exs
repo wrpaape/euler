c0754bf6e03059f55f955b5fd3098782c72bedfb
Reid Paape <wrpaape@gmail.com>
2016-01-14 12:46:23 -0600

    cheater cheater

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-config :euler, [problems_per_module: 25]
+config :euler, [problems_per_module: 10]
 
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this

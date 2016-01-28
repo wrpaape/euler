2d48720c62a349e08420c798e539234a6ec32705
Reid Paape <wrpaape@gmail.com>
2016-01-27 19:25:57 -0600

    ripping up function dispatch

--- lib/clang_api/include/prob_set.h
+++ lib/clang_api/include/prob_set.h
@@ -10,26 +10,27 @@
 /************************************************************************************
  *                               INITIAL DECLARATIONS                               *
  ************************************************************************************/
-typedef struct ProbNode {
-  unsigned int num;    /* problem number identifier */
-  struct ProbNode *next_ptr;  /* points to next problem in set */
-  char *(*func)(void); /* pointer to problem function */
-} ProbType;
+static const unsigned int *PROB_SET[][] = {
+  {2, {12}}
+};
+/* typedef struct ProbNode { */
+/*   unsigned int num;    /1* problem number identifier *1/ */
+/*   struct ProbNode *next_ptr;  /1* points to next problem in set *1/ */
+/*   char *(*func)(void); /1* pointer to problem function *1/ */
+/* } ProbType; */
 
-typedef struct SetNode {
-  unsigned int num;           /* problem set number identifier */
-  struct SetNode *next_ptr;   /* points to next problem set */
-  struct ProbNode *prob_ptr;  /* points to first problem in set */
-} SetType;
+/* typedef struct SetNode { */
+/*   unsigned int num;           /1* problem set number identifier *1/ */
+/*   struct SetNode *next_ptr;   /1* points to next problem set *1/ */
+/*   struct ProbNode *prob_ptr;  /1* points to first problem in set *1/ */
+/* } SetType; */
 
-static const unsigned int SET_SIZE  = sizeof(SetType);
-static const unsigned int PROB_SIZE = sizeof(ProbType);
+/* static const unsigned int SET_SIZE  = sizeof(SetType); */
+/* static const unsigned int PROB_SIZE = sizeof(ProbType); */
+
 /************************************************************************************
  *                               FUNCTION PROTOTYPES                                *
  ************************************************************************************/
-extern SetType *init_prob_set(void);
-extern ProbType *init_prob(const unsigned int prob_num, ProbType *next_prob_ptr, char *(*prob_func)(void));
-extern SetType *init_set(const unsigned int set_num, SetType *next_set_ptr, ProbType *head_prob_ptr);
-
-
-
+/* extern SetType *init_prob_set(void); */
+/* extern ProbType *init_prob(const unsigned int prob_num, ProbType *next_prob_ptr, char *(*prob_func)(void)); */
+/* extern SetType *init_set(const unsigned int set_num, SetType *next_set_ptr, ProbType *head_prob_ptr); */

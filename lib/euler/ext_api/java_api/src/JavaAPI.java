03506fe112bd9c85b4070526fff8a66e15a19b17
Reid Paape <wrpaape@gmail.com>
2016-02-03 13:25:13 -0600

    java api now times own functions

--- lib/euler/ext_api/java_api/src/JavaAPI.java
+++ lib/euler/ext_api/java_api/src/JavaAPI.java
@@ -9,7 +9,15 @@ public class JavaAPI {
 
       Method problemMethod = setClass.getDeclaredMethod("problem" + args[1]);
 
+      long timeStart = System.nanoTime();
+
       problemMethod.invoke(null);
+
+      long timeStop = System.nanoTime();
+
+      long timeElapsed = (timeStop - timeStart) / 1_000; // convert ns → μs
+
+      System.out.format("\n%d", timeElapsed);
 
     } catch(ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
 

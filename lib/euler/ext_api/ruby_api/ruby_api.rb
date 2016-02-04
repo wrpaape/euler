7511c6966585d58938e7ff5e65976d8b1b96cf79
Reid Paape <wrpaape@gmail.com>
2016-02-03 20:02:32 -0600

    ruby api hooked up

--- lib/euler/ext_api/ruby_api/ruby_api.rb
+++ lib/euler/ext_api/ruby_api/ruby_api.rb
@@ -1,6 +1,23 @@
+=begin
+**************************************************************************************
+*                                    ruby_api.rb                                     *
+*                                                                                    *
+* Houses the main class of the Ruby API, responsible for communication between       *
+* Elixir Mix project 'euler' and problems solved in Ruby.                            *
+**************************************************************************************
+=end
 Dir["set_*.rb"].each {|file| require_relative file}
 
 class RubyAPI
+=begin
+**************************************************************************************
+*                                   - self.main -                                    *
+*                                                                                    *
+* Method responsible for fetching the problem set class specified by 'set_num',      *
+* calling its problem method specified by 'prob_num', printing the results to        *
+* stdout, and handling all possible errors encountered along the way.                *
+**************************************************************************************
+=end
   def self.main(set_num, prob_num)
     set_class_str = "Set#{set_num}"
 
@@ -22,7 +39,13 @@ class RubyAPI
       exit 1
     end
 
-    set_class.send(prob_meth)
+    time_start   = Time.now
+    solution     = set_class.send(prob_meth)
+    time_finish  = Time.now
+
+    time_elapsed = (time_finish - time_start) * 1_000_000 # convert s → μs
+
+    print(solution, "\n", time_elapsed.round)             # print solution to stdout
   end
 end
 

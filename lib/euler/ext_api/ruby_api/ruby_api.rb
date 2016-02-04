95140f61baa2b0d9a341e2410ca69ba00c8879b6
Reid Paape <wrpaape@gmail.com>
2016-02-03 18:38:12 -0600

    better ruby_api

--- lib/euler/ext_api/ruby_api/ruby_api.rb
+++ lib/euler/ext_api/ruby_api/ruby_api.rb
@@ -1,8 +1,28 @@
-Dir["set_*.rb"].each {|file| require_relative file }
+Dir["set_*.rb"].each {|file| require_relative file}
 
 class RubyAPI
   def self.main(set_num, prob_num)
-    Set2.problem_18
+    set_class_str = "Set#{set_num}"
+
+    unless Object.const_defined?(set_class_str)
+      # TODO print error msg to stderr
+      puts "awooga 1"
+      exit 1
+    end
+
+    set_class = Object.const_get(set_class_str)
+
+
+    prob_meth = "problem_#{prob_num}"
+
+    unless set_class.respond_to?(prob_meth)
+      # TODO print error msg to stderr
+      puts prob_meth
+      puts "awooga 2"
+      exit 1
+    end
+
+    set_class.send(prob_meth)
   end
 end
 

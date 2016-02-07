b2679725cd175d7943e6be97f143394db4378737
Reid Paape <wrpaape@gmail.com>
2016-02-06 20:19:12 -0600

    harder than first appeared

--- lib/euler/ext_api/ruby_api/set_2.rb
+++ lib/euler/ext_api/ruby_api/set_2.rb
@@ -31,63 +31,78 @@ class Set2
 **************************************************************************************
 =end
   def self.problem_18(*args)
-    # tri = [[75],
-    #        [95, 64],
-    #        [17, 47, 82],
-    #        [18, 35, 87, 10],
-    #        [20,  4, 82, 47, 65],
-    #        [19,  1, 23, 75,  3, 34],
-    #        [88,  2, 77, 73,  7, 63, 67],
-    #        [99, 65,  4, 28,  6, 16, 70, 92],
-    #        [41, 41, 26, 56, 83, 40, 80, 70, 33],
-    #        [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
-    #        [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
-    #        [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
-    #        [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
-    #        [63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
-    #        [ 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23]]
+    tri = [[75],
+           [95, 64],
+           [17, 47, 82],
+           [18, 35, 87, 10],
+           [20,  4, 82, 47, 65],
+           [19,  1, 23, 75,  3, 34],
+           [88,  2, 77, 73,  7, 63, 67],
+           [99, 65,  4, 28,  6, 16, 70, 92],
+           [41, 41, 26, 56, 83, 40, 80, 70, 33],
+           [41, 48, 72, 33, 47, 32, 37, 16, 94, 29],
+           [53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14],
+           [70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57],
+           [91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48],
+           [63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
+           [ 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23]]
 
     begin
       branches = [{acc_sum: 75, col: 0}] # holds column indices of branches sharing largest last sum
 
+      acc = 75
       last_row = tri.length - 1
 
       1.upto(last_row - 1) do |row|  
         child_row = tri[row]
+        max_sum = 0
+        candidates = []
 
-        child_nodes = branches.flat_map do |branch|
-          root_sum = branch[:sum]
+        branches.each do |branch|
           root_col = branch[:col]
 
-          forks = [{dir: :left,  step: 0},
-                   {dir: :right, step: 1}]
+          fork_tail_row = row + 1
 
-          forks.map! do |fork|
-            fork[:col]  = root_col + fork[:step]
-            fork[:head] = child_row[fork[:col]]
-            fork[:sum]  = fork[:head]
-          end
+          0.upto(1) do |step|
+            fork_child        = {step: step}
+            fork_child[:col]  = root_col + fork_child[:step]
+            fork_child[:head] = child_row[fork_child[:col]]
+            fork_child[:sum]  = fork_child[:head]
 
-          forks.each do |fork|
-            col = fork[:col]
-            (row + 1).upto(last_row) do |row|
-              col        += fork[:step]
-              fork[:sum] += tri[row][col]
+            col = fork_child[:col]
+            fork_tail_row.upto(last_row) do |row|
+              col              += fork_child[:step]
+              fork_child[:sum] += tri[row][col]
+            end
+
+            if fork_child[:sum] >= max_sum
+              max_sum               = fork_child[:sum]
+              fork_child[:root_sum] = branch[:acc_sum]
+              candidates.push(fork_child)
             end
           end
+        end
 
+        candidates.reject! { |fork_child| fork_child[:sum] < max_sum }
 
-          if forks.first[:sum] != forks.last[:sum]
-            forks = forks.max_by(1) { |fork| fork[:sum] }
-          end
-
-            forks.map do |fork|
-              {acc_sum: root_sum, col: root_col + fork[:step]}
-            end
+        puts "*********"
+        puts "on row #{row}"
+        branches = candidates.map do |fork_child| 
+          puts "  branching: #{fork_child[:step].zero? ? "left" : "right"}"
+          puts "  head:      #{fork_child[:head]}"
+          puts "  row_max:   #{child_row.max}"
+          puts "  sum:       #{fork_child[:sum]}"
+          acc += fork_child[:head]
+          {
+            acc_sum: fork_child[:root_sum] + fork_child[:head],
+            col:     fork_child[:col]
+          }
         end
       end
 
-      puts live_branches.inspect
+      puts "acc: #{acc}"
+
+      puts branches.inspect
 
     rescue Exception => e
       puts e.inspect

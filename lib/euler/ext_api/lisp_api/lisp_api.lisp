834a44f0cdb6d777ac359ee5a6cf6cbcc5d6c2ca
Reid Paape <wrpaape@gmail.com>
2016-02-17 18:51:19 -0600

    aaaaaay stdio argv

--- lib/euler/ext_api/lisp_api/lisp_api.lisp
+++ lib/euler/ext_api/lisp_api/lisp_api.lisp
@@ -1,1 +1,19 @@
-(print "hello world")
+(defun main (set-num-str prob-num-str)
+  (let ((set-num  (intern set-num-str))
+        (prob-num (intern prob-num-str)))
+
+  (print `(set number ,set-num))
+  (print `(problem number ,prob-num))))
+
+(defun get-argv ()
+  (or
+   #+SBCL      *posix-argv*  
+   #+LISPWORKS system:*line-arguments-list*
+   #+CMU       extensions:*command-line-words
+   nil))
+
+
+(and
+  (defparameter *argv* (get-argv))
+  (print *argv*)
+  (apply #'main (cdr *argv*)))

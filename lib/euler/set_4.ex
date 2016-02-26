74d585c2063a8e578330d4aff1f82f80252e06dd
Reid Paape <wrpaape@gmail.com>
2016-02-26 17:48:18 -0600

    attempting 32 in java

--- lib/euler/set_4.ex
+++ lib/euler/set_4.ex
@@ -1,5 +1,6 @@
 defmodule Euler.Set4 do
-  alias Euler.ExtAPI.{LispAPI}
+  alias Euler.ExtAPI.{LispAPI,
+                      JavaAPI}
 
   @moduledoc """
   Set4 houses solutions to problems 31 - 40.
@@ -24,4 +25,18 @@ defmodule Euler.Set4 do
   73682
   """
   def problem_31, do: LispAPI.call(~w(4 31))
+
+
+  @doc """
+  32) Pandigital Products
+
+  We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+
+  The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+  Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+
+  HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+  """
+  def problem_32, do: JavaAPI.call(~w(4 32))
 end
